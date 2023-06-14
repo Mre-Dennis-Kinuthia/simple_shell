d6378ab730e10d87df5599424d1b0843b541ab51
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-06-14 18:23:02 +0300

    fix

--- shell.c
+++ shell.c
@@ -1,20 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
-/**
- * main - execve example
- *
- * @argc: number of arguments
- * @argv: argument vectors
- * Return: Always 0.
- */
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-    char *buffer, **array, c;
+    char *buffer, **array;
     int counter = 0;
 
-    (void)argc;
     while (1)
     {
         counter++;
@@ -23,92 +16,104 @@ int main(int argc, char **argv)
 
         buffer = _getline();
 
-        c = buffer[0];
-
-        if (c == '\0')
-            continue;
-        array = parser(buffer);
-        if (check_cmd(array[0]) == 0)
+        if (buffer[0] == '\0')
         {
-            if (strcmp(array[0], "exit") == 0)
-            {
-                free(array);
-                free(buffer);
-                exit(0);
-            }
-            exec_builtin(array, counter, argv[0]);
-            free(array);
             free(buffer);
-            array = NULL;
-            buffer = NULL;
             continue;
         }
+
+        array = parser(buffer);
+
+        if (array == NULL)
+        {
+            free(buffer);
+            continue;
+        }
+
+        if (strcmp(array[0], "exit") == 0)
+        {
+            int status = 0;
+            if (array[1] != NULL)
+                status = atoi(array[1]);
+
+            free(buffer);
+            free(array);
+            exit(status);
+        }
+
+        if (check_cmd(array[0]) == 0)
+        {
+            exec_builtin(array, counter, argv[0]);
+        }
         else
+        {
             execute(array, counter, argv, buffer);
+        }
+
         free(array);
         free(buffer);
     }
-    return (0);
+
+    return 0;
 }
 
-/**
- * parser - parses the command input
- *
- * @buffer: string containing command
- *
- * Return: Parsed command
- */
 char **parser(char *buffer)
 {
     char **cmd, *token;
-    int i;
+    int i = 0;
 
     if (buffer == NULL)
-        return (NULL);
-
-    token = strtok(buffer, " \n");
-    if (token == NULL)
-        return (NULL);
+        return NULL;
 
     cmd = malloc(sizeof(char *) * 1024);
-
-    i = 0;
-    while (token)
+    if (cmd == NULL)
     {
-        cmd[i++] = token;
+        perror("malloc failed");
+        return NULL;
+    }
+
+    token = strtok(buffer, " \n");
+    while (token != NULL)
+    {
+        cmd[i] = strdup(token);
+        if (cmd[i] == NULL)
+        {
+            perror("strdup failed");
+            free(cmd);
+            return NULL;
+        }
+        i++;
         token = strtok(NULL, " \n");
     }
     cmd[i] = NULL;
-    return (cmd);
+
+    return cmd;
 }
 
-/**
- * execute - executes commands within the shell
- *
- * @array: parsed command
- * @counter: command number
- * @argv: command line arguments
- * @buffer: buffer containing input
- */
 void execute(char **array, int counter, char **argv, char *buffer)
 {
+    pid_t pid;
     int status;
 
-    if (fork() != 0)
+    pid = fork();
+    if (pid == -1)
     {
-        wait(&status);
+        perror("fork failed");
+        return;
     }
-    else
+    else if (pid == 0)
     {
         if (_strncmp(array[0], "./", 2) != 0 && _strncmp(array[0], "/", 1) != 0)
             path_finder(&array[0]);
 
-        if (execve(*array, array, environ) == -1)
+        if (execve(array[0], array, environ) == -1)
         {
             printE(counter, array[0], argv[0]);
-            free(array);
-            free(buffer);
             exit(EXIT_FAILURE);
         }
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
     }
 }
